3e68e6c2e7c078914ca081ffb9b984e75e945409
Marko Krizmancic <marko.krizmancic@fer.hr>
2023-04-21 19:06:38 +0200

    Automate startup

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -1,4 +1,10 @@
-sleep 120
-source /boot/orange_box.config
-sudo nmcli -w 150 dev wifi connect "$SSID" password "$PASS"
-echo success > /home/rock/orange_box/status/wifi_connect_success.txt
+WIFI_FILE="/home/rock/OrangeBox/status/wifi_connect_success.txt"
+if [ ! -f "$WIFI_FILE" ]; then
+    sleep 120
+    source /boot/orange_box.config
+    sudo nmcli -w 180 dev wifi connect "$SSID" password "$PASS" && \
+    echo success > "$WIFI_FILE" && \
+    echo "Successfully created WIFI connection."
+else
+    echo "WIFI connection was already created."
+fi
---
+++ scripts/startup.sh
@@ -0,0 +1,11 @@
+# Set working directory.
+cd /home/rock/OrangeBox
+
+# Connect to WIFI.
+# bash scripts/connect.sh
+
+# Check internet connection and update all repositories.
+bash scripts/update_drivers.sh
+
+# Run the main tmuxinator file.
+tmuxinator start -p start_all.yaml
