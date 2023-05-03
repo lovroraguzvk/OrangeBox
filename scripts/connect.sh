388596f8d99076d8be16161c97e5f544114052fc
Marko Krizmancic <marko.krizmancic@fer.hr>
2023-05-03 17:34:19 +0200

    Convert USB file to Unix and sleep for network

--- scripts/connect.sh
+++ scripts/connect.sh
@@ -7,13 +7,14 @@ fi
 
 if [ ! -f "$WIFI_FILE" ]; then
     sleep 120
-    bash scripts/copy_usb.sh
+    bash scripts/copy_usb.sh "$CONFIG_FILE"
     source "$CONFIG_FILE"
     sudo nmcli -w 180 dev wifi connect "$SSID" password "$PASS" && \
     echo success > "$WIFI_FILE" && \
     echo "Successfully created WIFI connection." && \
     bash scripts/set_leds.sh
 else
+    sleep 30
     echo "WIFI connection was already created."
     bash scripts/set_leds.sh
 fi
--- scripts/copy_usb.sh
+++ scripts/copy_usb.sh
@@ -1,4 +1,12 @@
+CONFIG_FILE=$1
+
+if [[ -z $CONFIG_FILE ]]; then
+    echo "Provide file name as first positional argument."
+    exit 1
+fi
+
 sudo mkdir /media/usb
 sudo mount /dev/sda1 /media/usb
-cp /media/usb/orange_box.config /home/rock/OrangeBox/config/orange_box.config
+cp /media/usb/orange_box.config "$CONFIG_FILE"
+dos2unix "$CONFIG_FILE"
 sudo umount /media/usb
