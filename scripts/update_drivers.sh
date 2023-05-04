d56958739d1a1c6c3bdcb5fc9b455e5af95e0fed
mkrizmancic <marko.krizmancic@fer.hr>
2023-05-04 15:12:03 +0200

    Fix typo

--- scripts/update_drivers.sh
+++ scripts/update_drivers.sh
@@ -4,7 +4,7 @@ readonly PING_URL="https://www.github.com"
 wget -q --tries=5 --timeout=2 --spider "$PING_URL"
 if [ $? -eq 0 ]; then
     echo "Internet connection is available."
-    git pull orgin main
+    git pull origin main
     /home/rock/.local/bin/gitman install
 else
     echo "Internet connection unavailable. Skipping update."
