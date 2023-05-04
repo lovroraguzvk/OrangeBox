41584a46874f7852be762a10ab869fc7f09bc793
mkrizmancic <marko.krizmancic@fer.hr>
2023-05-04 15:26:26 +0200

    Get correct IP address

--- interface/setting_receiver.py
+++ interface/setting_receiver.py
@@ -2,7 +2,16 @@ import zmq
 import socket
 from subprocess import Popen
 
-ipaddr = socket.gethostbyname(socket.gethostname()) 
+
+def get_ip_address():
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))
+        return s.getsockname()[0]
+    except OSError:
+        return "127.0.0.1"
+    
+ipaddr = get_ip_address()
 context = zmq.Context()
 zsocket = context.socket(zmq.SUB)
 zsocket.bind(f"tcp://{ipaddr}:5557")
