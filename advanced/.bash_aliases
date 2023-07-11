839f3ed12c89714162056eb4005feb536dd3ab49
Marko Krizmancic <marko.krizmancic@fer.hr>
2023-07-11 12:02:28 +0200

    Add advanced files for manual setup

---
+++ advanced/.bash_aliases
@@ -0,0 +1,4 @@
+alias re-source='source ~/.bashrc'
+alias cd..='cd ..'
+alias ra='. ranger'
+alias gitc='git log --graph --pretty=oneline --abbrev-commit --all'
---
+++ advanced/copy_config.sh
@@ -0,0 +1,11 @@
+mv ~/.tmux.conf ~/.tmux.conf.backup
+mv ~/.nanorc ~/.nanorc.backup
+mv ~/.bash_aliases ~/.bash_aliases.backup
+mv ~/.config/htop ~/.config/htop.backup
+mv ~/.config/ranger ~/.config/ranger.backup
+
+cp .tmux.conf ~/
+cp .nanorc ~/
+cp .bash_aliases ~/
+cp -R ranger ~/.config/
+cp -R htop ~/.config/
