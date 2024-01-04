8e192256da4147654a688e4f3ef005c5fe0cd3cf
mkrizmancic <marko.krizmancic@fer.hr>
2024-01-04 19:25:05 +0100

    Remove code from docs

--- scripts/clean_up.sh
+++
@@ -1,5 +0,0 @@
-rm ~/OrangeBox/config/orange_box.config
-rm -rf ~/OrangeBox/status/
-rm -rf ~/OrangeBox/drivers/mu_interface/mu_interface/Sensor/logs
-rm -rf ~/measurements/*
-sudo rm /etc/NetworkManager/system/system-connections/*
--- scripts/set_leds.sh
+++
@@ -1,2 +0,0 @@
-sudo sh -c "echo 0 > /sys/class/leds/rockpis\:blue\:user/brightness"
-sudo sh -c "echo heartbeat > /sys/class/leds/rockpis\:green\:power/trigger"
