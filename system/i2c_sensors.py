c0b217a203eb1b19e55c77902ace7f5521eac6a3
Marko Krizmancic <marko.krizmancic@fer.hr>
2024-02-21 14:52:46 +0100

    Reformat

--- system/i2c_sensors.py
+++ system/i2c_sensors.py
@@ -16,19 +16,25 @@ from system.telegram_bot.telegram_bot import broadcast_message
 
 ## Parse arguments.
 parser = argparse.ArgumentParser(description="Arguments for the sensor node.")
-parser.add_argument('--int', action='store', type=int, default=5,
-    help="Time interval between two measurements (in seconds).")
-parser.add_argument('--addr', action='store', default='localhost',
-    help="Address of the MQTT subscriber. Can be IP, localhost, *.local, etc.")
-parser.add_argument('--dir', action='store', default=Path.home() / 'measurements/',
-    help="Directory where measurement data is saved.")
+parser.add_argument(
+    "--int", action="store", type=int, default=5, help="Time interval between two measurements (in seconds)."
+)
+parser.add_argument(
+    "--addr",
+    action="store",
+    default="localhost",
+    help="Address of the MQTT subscriber. Can be IP, localhost, *.local, etc.",
+)
+parser.add_argument(
+    "--dir", action="store", default=Path.home() / "measurements/", help="Directory where measurement data is saved."
+)
 args = parser.parse_args()
 
 
 hostname = socket.gethostname()
 
 
-## Set up I2C sensors. 
+## Set up I2C sensors.
 i2c_bus = busio.I2C(board.SCL1, board.SDA1)
 ina219_solar = INA219(i2c_bus, addr=0x40)
 ina219_battery = INA219(i2c_bus, addr=0x41)
@@ -37,7 +43,7 @@ try:
     temp_is_available = True
 except ValueError:
     temp_is_available = False
-    
+
 
 # optional : change configuration to use 32 samples averaging for both bus voltage and shunt voltage
 ina219_solar.bus_adc_resolution = ADCResolution.ADCRES_12BIT_32S
@@ -70,23 +76,30 @@ while True:
 
     if bus_voltage_battery < 3.2:
         broadcast_message("Battery Voltage Is Low. Shutting Down!")
-    
+
     temperature = 0
     humidity = 0
     if temp_is_available:
         try:
-            temperature = round(shtc3.temperature, 2)                 # temperature in degrees Celsius
-            humidity = round(shtc3.relative_humidity, 2)              # relative humidity in %
+            temperature = round(shtc3.temperature, 2)  # temperature in degrees Celsius
+            humidity = round(shtc3.relative_humidity, 2)  # relative humidity in %
             print(f"Temperature: {temperature} °C, Humidity: {humidity} %")
         except Exception as e:
             print(f"Error reading temperature and humidity: {e}")
 
     # Publish data over ZMQ.
-    payload = np.array([int(datetime.now().timestamp()), 
-                        bus_voltage_solar, current_solar, 
-                        bus_voltage_battery, current_battery, 
-                        temperature, humidity])
-    
+    payload = np.array(
+        [
+            int(datetime.now().timestamp()),
+            bus_voltage_solar,
+            current_solar,
+            bus_voltage_battery,
+            current_battery,
+            temperature,
+            humidity,
+        ]
+    )
+
     # Create a new csv file after the specified interval.
     current_time = datetime.now()
     if current_time.hour in {0, 12} and current_time.hour != last_time.hour:
@@ -95,11 +108,11 @@ while True:
         csv_object = data2csv(file_path, file_name, "energy")
         csv_object.fix_ownership()
         last_time = current_time
-    
+
     # Store data to csv file locally.
     data = payload.tolist()
     csv_object.write2csv(data)
-    
+
     # TODO: add some statistics to print out
 
     # Check internal calculations haven't overflowed (doesn't detect ADC overflows)
--- system/telegram_bot/telegram_bot/telegram_bot.py
+++ system/telegram_bot/telegram_bot/telegram_bot.py
@@ -1,62 +1,49 @@
-import requests
-import json
-import telebot
-from telebot import types
-import pandas as pd
-import matplotlib.pyplot as plt
-from io import BytesIO
 import os
-import shutil
-import plotly.graph_objects as go
-from plotly.subplots import make_subplots
-import plotly.express as px
 import pathlib
-import subprocess
-import dash
-import dash_bootstrap_components as dbc
-from datetime import datetime
+import shutil
 from time import sleep
 
+import pandas as pd
+import plotly.graph_objects as go
+import telebot
+from plotly.subplots import make_subplots
 
-bot_token = os.getenv('BOT_TOKEN')
+bot_token = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(bot_token)
 
+# You can import the bot from anywhere and use it to send messages,
+# but only one bot can receive messages from users.
+# https://github.com/eternnoir/pyTelegramBotAPI/issues/1253#issuecomment-894232944
 def broadcast_message(message):
     with open("/home/rock/OrangeBox/system/telegram_bot/telegram_bot/IDs.txt", "r") as file:
         for id in file.readlines():
             bot.send_message(id, message)
             print("Message sent")
 
-@bot.message_handler(commands=['start'])
-def handle_start(message):
-    welcome_message = """
-Welcome to the Telegram Bot!
+welcome_message = """
+    Welcome to the Telegram Bot!
 
-Here are the available commands:
-/start - Start the bot and get a welcome message.
-/meas_files - Get .zip folder of all measurements.
-/power_plot - Generate a plot based on the data.
-/subscribe - Recieve update messages from the bot.
-/unsubscribe - Unsubscribe from update messages.
-
-Feel free to explore the bot's functionalities! If you have any questions, use the /help command.
+    Here are the available commands:
+    /start - Start the bot and get a welcome message.
+    /meas_files - Get .zip folder of all measurements.
+    /power_plot - Generate a plot based on the data.
+    /subscribe - Recieve update messages from the bot.
+    /unsubscribe - Unsubscribe from update messages.
 """
-    bot.send_message(message.chat.id, welcome_message)
-    
 
-@bot.message_handler(commands=['help'])
+# Handlers receiving messages. 
+@bot.message_handler(commands=["start"])
 def handle_start(message):
-    welcome_message = """
-Here are the available commands:
-/start - Start the bot and get a welcome message.
-/meas_files - Get .zip folder of all measurements.
-/power_plot - Generate a plot based on the data.
-/subscribe - Recieve update messages from the bot.
-/unsubscribe - Unsubscribe from update messages.
-"""
+    help_message = "Feel free to explore the bot's functionalities! If you have any questions, use the /help command."
+    bot.send_message(message.chat.id, f"welcome_message\n\n{help_message}")
+
+
+@bot.message_handler(commands=["help"])
+def handle_help(message):
     bot.send_message(message.chat.id, welcome_message)
 
-@bot.message_handler(commands=['subscribe']) # subs
+
+@bot.message_handler(commands=["subscribe"])  # subs
 def handle_add_id(message):
     # Open a file in write mode ("w" or "a" for append)
     existing_ids = []
@@ -65,7 +52,7 @@ def handle_add_id(message):
             existing_ids.append(line.strip())
     with open("IDs.txt", "a+") as file:
         # List of strings to store in the file
-        #existing_ids = [line.strip() for line in file.readlines()]
+        # existing_ids = [line.strip() for line in file.readlines()]
         print(existing_ids)
         id = message.chat.id
         if str(id) not in existing_ids:
@@ -74,34 +61,36 @@ def handle_add_id(message):
         else:
             bot.reply_to(message, "Already subscribed")
 
-@bot.message_handler(commands=['unsubscribe']) # unsubs
+
+@bot.message_handler(commands=["unsubscribe"])  # unsubs
 def handle_remove_id(message):
     # Open a file in write mode ("w" or "a" for append)
     with open("IDs.txt", "r") as file:
         # Read all lines from the file into a list
         lines = file.readlines()
-    
+
     lines = [line for line in lines if line.strip() not in str(message.chat.id)]
     with open("IDs.txt", "w") as file:
         # Write the modified lines back to the file
         file.writelines(lines)
     bot.reply_to(message, "Unsubscribed")
 
-@bot.message_handler(commands=['meas_files']) 
+
+@bot.message_handler(commands=["meas_files"])
 def handle_file(message):
-    folder_path = '/home/rock/measurements/'  
+    folder_path = "/home/rock/measurements/"
     try:
-        shutil.make_archive('measurements', 'zip', folder_path)
-        with open('measurements.zip', 'rb') as file:
+        shutil.make_archive("measurements", "zip", folder_path)
+        with open("measurements.zip", "rb") as file:
             bot.send_document(message.chat.id, file)
-        os.remove('measurements.zip')
+        os.remove("measurements.zip")
     except Exception as e:
         print(f"Error: {e}")
 
 
-@bot.message_handler(commands=['power_plot']) # odvojiti plot_meas i plot_sens
+@bot.message_handler(commands=["power_plot"])  # odvojiti plot_meas i plot_sens
 def send_plot(message):
-    file_path = 'measurements.xlsx'  # Replace with the actual path to your Excel file
+    file_path = "measurements.xlsx"  # Replace with the actual path to your Excel file
     try:
         MEASUREMENT_PATH = pathlib.Path.home() / "measurements"
         ENERGY_PATH = MEASUREMENT_PATH / "Power"
@@ -112,51 +101,71 @@ def send_plot(message):
         df = pd.read_csv(ENERGY_PATH / file_names[-1])
         # TODO: change back to datetime
         df["datetime"] = pd.to_datetime(df["timestamp"])  # convert to datetime object
-        df_window = df.loc[df['datetime'] > pd.Timestamp.now() - pd.Timedelta(hours=time_select)]
+        df_window = df.loc[df["datetime"] > pd.Timestamp.now() - pd.Timedelta(hours=time_select)]
 
         fig_power = make_subplots(specs=[[{"secondary_y": True}]])
         # Add traces
         fig_power.add_trace(
-            go.Scatter(x=df_window["datetime"], y=df_window["bus_voltage_solar"],
-            name="bus_voltage_solar", mode='lines', line_color="red", line = dict(dash='dash')), secondary_y=False,
+            go.Scatter(
+                x=df_window["datetime"],
+                y=df_window["bus_voltage_solar"],
+                name="bus_voltage_solar",
+                mode="lines",
+                line_color="red",
+                line=dict(dash="dash"),
+            ),
+            secondary_y=False,
         )
 
         fig_power.add_trace(
-            go.Scatter(x=df_window["datetime"], y=df_window["current_solar"], name="current_solar", mode='lines', line_color="blue", line = dict(dash='dash')),
+            go.Scatter(
+                x=df_window["datetime"],
+                y=df_window["current_solar"],
+                name="current_solar",
+                mode="lines",
+                line_color="blue",
+                line=dict(dash="dash"),
+            ),
             secondary_y=True,
         )
 
         fig_power.add_trace(
-            go.Scatter(x=df_window["datetime"], y=df_window["bus_voltage_battery"],
-            name="bus_voltage_battery", mode='lines', line_color="red"), secondary_y=False,
+            go.Scatter(
+                x=df_window["datetime"],
+                y=df_window["bus_voltage_battery"],
+                name="bus_voltage_battery",
+                mode="lines",
+                line_color="red",
+            ),
+            secondary_y=False,
         )
 
         fig_power.add_trace(
-            go.Scatter(x=df_window["datetime"], y=df_window["current_battery"], name="current_battery", mode='lines', line_color="blue"),
+            go.Scatter(
+                x=df_window["datetime"],
+                y=df_window["current_battery"],
+                name="current_battery",
+                mode="lines",
+                line_color="blue",
+            ),
             secondary_y=True,
         )
 
-            # Add figure title
+        # Add figure title
         fig_power.update_layout(title_text="Energy Consumption")
 
         # Set x-axis title
         fig_power.update_xaxes(title_text="datetime")
 
         # Set y-axes titles
-        fig_power.update_yaxes(
-            title_text="voltage [V]", 
-            color="red",
-            secondary_y=False)
-        fig_power.update_yaxes(
-            title_text="current [mA]", 
-            color="blue",
-            secondary_y=True)
+        fig_power.update_yaxes(title_text="voltage [V]", color="red", secondary_y=False)
+        fig_power.update_yaxes(title_text="current [mA]", color="blue", secondary_y=True)
 
-        image_path = 'power_plot.png'
+        image_path = "power_plot.png"
         fig_power.write_image(image_path)
 
         # Send the image via Telegram
-        with open(image_path, 'rb') as image:
+        with open(image_path, "rb") as image:
             bot.send_photo(message.chat.id, image)
 
         # Remove the temporary image file
@@ -165,7 +174,8 @@ def send_plot(message):
     except Exception as e:
         print(f"Error: {e}")
 
-if __name__=='__main__': # neki flag za postojanje bota (while)
+
+if __name__ == "__main__":  # neki flag za postojanje bota (while)
     while True:
         sleep(0.1)
         try:
@@ -174,4 +184,3 @@ if __name__=='__main__': # neki flag za postojanje bota (while)
         except Exception as e:
             print(f"Error: {e}")
             # Handle the error as needed, e.g., sleep and retry.
-
